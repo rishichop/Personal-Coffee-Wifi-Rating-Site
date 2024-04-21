cd82269ff9602078f8997db131c0ff4d50294428
Rishikesh Chopade <84148892+rishichop@users.noreply.github.com>
2024-04-21 22:04:33 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -3,24 +3,50 @@ from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, URLField, TimeField, RadioField
 from wtforms.validators import DataRequired
-import csv
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cafe.db"
+
+db.init_app(app)
+
+
+class cafe(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    cafe_name: Mapped[str] = mapped_column(String(250), nullable=False)
+    location_url: Mapped[str] = mapped_column(String(250), nullable=False)
+    open_time: Mapped[str] = mapped_column(String(10), nullable=False)
+    close_time: Mapped[str] = mapped_column(String(10), nullable=False)
+    coffee_rating: Mapped[str] = mapped_column(String(50), nullable=False)
+    wifi_rating: Mapped[str] = mapped_column(String(50), nullable=False)
+    power_rating: Mapped[str] = mapped_column(String(50), nullable=False)
+
+
+# with app.app_context():
+#     db.create_all()
+#     db.session.add(cafe(
+#         cafe_name="Rishi cafe",
+#         location_url="Somewhere",
+#         open_time="12:00",
+#         close_time="12:00",
+#         coffee_rating="☕☕☕",
+#         wifi_rating="💪💪💪💪💪",
+#         power_rating="🔌🔌"
+#         ))
+#     db.session.commit()
 
 
 class CafeForm(FlaskForm):
@@ -48,16 +74,7 @@ class CafeForm(FlaskForm):
                               validators=[DataRequired()],
                               choices=["🔌", "🔌🔌", "🔌🔌🔌", "🔌🔌🔌🔌", "🔌🔌🔌🔌🔌", "✘"])
 
-    submit = SubmitField('Submit')
-
-
-# Exercise:
-# add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
-# make coffee/wifi/power a select element with choice of 0 to 5.
-# e.g. You could use emojis ☕️/💪/✘/🔌
-# make all fields required except submit
-# use a validator to check that the URL field has a URL entered.
-# ---------------------------------------------------------------------------
+    submit = SubmitField(label='Add new Cafe')
 
 
 # all Flask routes below
@@ -70,29 +87,33 @@ def home():
 def add_cafe():
     form = CafeForm()
     if form.validate_on_submit():
-        with open('cafe-data.csv', newline='', encoding="utf-8", mode="a") as csv_file:
-            csv_file.write('\n')
-            csv_file.write(f"{form.cafe.data},"
-                           f"{form.location_url.data},"
-                           f"{form.open.data},"
-                           f"{form.close.data},"
-                           f"{form.coffee_rate.data},"
-                           f"{form.wifi_rate.data},"
-                           f"{form.power_rating.data}")
-        return redirect('/cafes')
-    # Exercise:
-    # Make the form write a new row into cafe-data.csv
-    # with   if form.validate_on_submit()
+        with app.app_context():
+            db.session.add(cafe(
+                cafe_name=str(form.cafe.data),
+                location_url=str(form.location_url.data),
+                open_time=str(form.open.data),
+                close_time=str(form.close.data),
+                coffee_rating=str(form.coffee_rate.data),
+                wifi_rating=str(form.wifi_rate.data),
+                power_rating=str(form.power_rating.data)
+            ))
+            db.session.commit()
+        return redirect("/cafes")
     return render_template('add.html', form=form)
 
 
+# @app.route('/cafes')
+# def cafes():
+#     with open('cafe-data.csv', newline='', encoding='utf-8') as csv_file:
+#         csv_data = csv.reader(csv_file, delimiter=',')
+#         list_of_rows = []
+#         for row in csv_data:
+#             list_of_rows.append(row)
+#     return render_template('cafes.html', cafes=list_of_rows)
+
 @app.route('/cafes')
 def cafes():
-    with open('cafe-data.csv', newline='', encoding='utf-8') as csv_file:
-        csv_data = csv.reader(csv_file, delimiter=',')
-        list_of_rows = []
-        for row in csv_data:
-            list_of_rows.append(row)
+    list_of_rows = db.session.execute(db.select(cafe).order_by(cafe.id)).scalars()
     return render_template('cafes.html', cafes=list_of_rows)
 
 
